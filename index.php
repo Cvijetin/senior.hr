1da4ffd9db9b402b25203beb725e2611c8aa1f6e
Cvijetin <vidakovic.cvijetin@gmail.com>
2019-08-13 16:28:28 +0200

    inital commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <header class="header-background">
+        <h1>senior.hr</h1>
+    </header>
+    <main>
+        <section class="section-wrapper">
+            <div class="topic-wrapper left">
+                <img src="assets/news.svg" class="topic-icon" alt="Vijesti">
+                <h2>Vijesti</h2>
+            </div>
+            <div class="topic-wrapper right">
+                <img src="assets/weather.svg" class="topic-icon" alt="Vrijeme">
+                <h2>Vrijeme</h2>
+            </div>
+        </section>
+        <section class="section-wrapper">
+            <div class="topic-wrapper left">
+                <img src="assets/taxi.svg" class="topic-icon" alt="Prijevoz">
+                <h2>Prijevoz</h2>
+            </div>
+            <div class="topic-wrapper right">
+                <img src="assets/health.svg" class="topic-icon" alt="Zdravlje">
+                <h2>Zdravlje</h2>
+            </div>
+        </section>
+        <section class="section-wrapper">
+            <div class="topic-wrapper left">
+                <img src="assets/events.svg" class="topic-icon" alt="Događaji">
+                <h2>Događaji</h2>
+            </div>
+            <div class="topic-wrapper right">
+                <img src="assets/suggestion.svg" class="topic-icon" alt="Prijedlozi">
+                <h2>Prijedlozi</h2>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
