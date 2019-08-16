0ce8d662c86e65c6c7bb05189b2a7d944a0ed4a3
Cvijetin <vidakovic.cvijetin@gmail.com>
2019-08-16 23:10:37 +0200

    news feed design update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>senior.hr</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
@@ -16,32 +16,46 @@
     <main>
         <section class="section-wrapper">
             <div class="topic-wrapper left">
-                <img src="assets/news.svg" class="topic-icon" alt="Vijesti">
-                <h2>Vijesti</h2>
+                <a href="news/landing.php">
+                    <div class="topic-icon-wrapper">
+                        <img src="assets/news.svg" class="topic-icon" alt="Vijesti">
+                        <h2>Vijesti</h2>
+                    </div>
+                </a>
             </div>
             <div class="topic-wrapper right">
-                <img src="assets/weather.svg" class="topic-icon" alt="Vrijeme">
-                <h2>Vrijeme</h2>
+                <div class="topic-icon-wrapper">
+                    <img src="assets/weather.svg" class="topic-icon" alt="Vrijeme">
+                    <h2>Vrijeme</h2>
+                </div>
             </div>
         </section>
         <section class="section-wrapper">
             <div class="topic-wrapper left">
-                <img src="assets/taxi.svg" class="topic-icon" alt="Prijevoz">
-                <h2>Prijevoz</h2>
+                <div class="topic-icon-wrapper">
+                    <img src="assets/taxi.svg" class="topic-icon" alt="Prijevoz">
+                    <h2>Prijevoz</h2>
+                </div>
             </div>
             <div class="topic-wrapper right">
-                <img src="assets/health.svg" class="topic-icon" alt="Zdravlje">
-                <h2>Zdravlje</h2>
+                <div class="topic-icon-wrapper">
+                    <img src="assets/health.svg" class="topic-icon" alt="Zdravlje">
+                    <h2>Zdravlje</h2>
+                </div>
             </div>
         </section>
         <section class="section-wrapper">
             <div class="topic-wrapper left">
-                <img src="assets/events.svg" class="topic-icon" alt="Događaji">
-                <h2>Događaji</h2>
+                <div class="topic-icon-wrapper">
+                    <img src="assets/events.svg" class="topic-icon" alt="Događaji">
+                    <h2>Događaji</h2>
+                </div>
             </div>
             <div class="topic-wrapper right">
-                <img src="assets/suggestion.svg" class="topic-icon" alt="Prijedlozi">
-                <h2>Prijedlozi</h2>
+                <div class="topic-icon-wrapper">
+                    <img src="assets/suggestion.svg" class="topic-icon" alt="Prijedlozi">
+                    <h2>Prijedlozi</h2>
+                </div>
             </div>
         </section>
     </main>
---
+++ news/landing.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>senior.hr</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+
+<body>
+    <header class="header-background">
+        <h1>Vijesti</h1>
+    </header>
+
+    <main>
+    <section class="section-wrapper">
+            <div class="topic-wrapper left">
+            <a href="indexFeed.php">
+                   <p>Index.hr</p>
+               </a>
+            </div>
+            <div class="topic-wrapper right">
+               <a href="24sataFeed.php">
+                   <p>24 sata</p>
+               </a>
+            </div>
+        </section>
+    </main>
+
+</body>
+</html>
